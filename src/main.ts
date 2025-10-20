6127d379ddcd4462afc4a87fd97bcefcdf611355
Kyle Carberry <kyle@carberry.com>
2025-10-20 19:25:29 -0400

    Simplify: handle Electron spawning in main.ts instead of separate bin wrapper

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
 
-const isServer = process.argv.length > 2 && process.argv[2] === "server";
+const isServer = process.argv.includes("server");
+const isElectron = process.versions.electron !== undefined;
 
 if (isServer) {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./main-server");
-} else {
+} else if (isElectron) {
+  // Already running in Electron, launch desktop
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./main-desktop");
+} else {
+  // Running in Node, need to spawn Electron
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { spawn } = require("child_process");
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const path = require("path");
+  
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const electron = require("electron");
+  const appPath = path.join(__dirname, "..");
+  
+  const child = spawn(electron, [appPath, ...process.argv.slice(2)], {
+    stdio: "inherit",
+  });
+  
+  child.on("close", (code) => {
+    process.exit(code);
+  });
 }
