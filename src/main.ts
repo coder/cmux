53636bfee8735ffdac07e0eb8c4c87e854594f33
Kyle Carberry <kyle@carberry.com>
2025-10-20 19:26:24 -0400

    Revert electron spawning logic - will fix later

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,11 @@
 #!/usr/bin/env node
 
-const isServer = process.argv.includes("server");
-const isElectron = process.versions.electron !== undefined;
+const isServer = process.argv.length > 2 && process.argv[2] === "server";
 
 if (isServer) {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./main-server");
-} else if (isElectron) {
-  // Already running in Electron, launch desktop
+} else {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./main-desktop");
-} else {
-  // Running in Node, need to spawn Electron
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { spawn } = require("child_process");
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const path = require("path");
-  
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const electron = require("electron");
-  const appPath = path.join(__dirname, "..");
-  
-  const child = spawn(electron, [appPath, ...process.argv.slice(2)], {
-    stdio: "inherit",
-  });
-  
-  child.on("close", (code) => {
-    process.exit(code);
-  });
 }
