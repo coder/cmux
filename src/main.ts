291d12db98383a7115e21e176af970a93e25646a
Jon Ayers <jon@coder.com>
2025-11-14 18:31:29 -0500

    omit 'server' arg to main-server

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@
 const isServer = process.argv.length > 2 && process.argv[2] === "server";
 
 if (isServer) {
+  // Remove 'server' from args since main-server doesn't expect it as a positional argument.
+  process.argv.splice(2, 1);
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   require("./main-server");
 } else {
